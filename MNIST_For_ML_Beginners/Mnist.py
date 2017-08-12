3bf57a1ea0a783f9c03a296ef150292c23974e1c
Mithun_Aggarwal <aggarwalmithun@gmail.com>
2017-08-12 13:22:44 +1000

    no message

--- MNIST_For_ML_Beginners/Mnist.py
+++ MNIST_For_ML_Beginners/Mnist.py
@@ -76,7 +76,7 @@ def main(_):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+  parser.add_argument('--data_dir', type=str, default='C:/Users/ksxl806/Documents/PythonScripts/TenserflowGit/MNIST_For_ML_Beginners/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
